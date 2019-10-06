696b804ab8d38e22302bdbeccea5f73c53469851
cindy <hsuan@192-168-1-114.tpgi.com.au>
2019-10-06 17:49:35 +1000

    firstcomitt

---
+++ index.js
@@ -0,0 +1,24 @@
+var mqtt = require('mqtt')
+
+var axios = require('axios')
+
+require("dotenv").config()
+
+var client  = mqtt.connect(`mqtt://${mqttBroker}:1883`)
+client.on('connect', function () {
+
+        client.subscribe('pulse',function(err){
+            if(!err){
+                console.log("Success");
+            }
+        })
+    
+})
+
+//message event 
+client.on('message', function (topic, message) {
+
+
+context = message.toString();
+console.log(context)
+})
